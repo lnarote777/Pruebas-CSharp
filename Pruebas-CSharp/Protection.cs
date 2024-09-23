740b96797dd4c547ca22152359a9f1f791893848
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-09-23 16:45:24 +0200

    documentacion

--- Pruebas-CSharp/Protection.cs
+++ Pruebas-CSharp/Protection.cs
@@ -11,9 +11,13 @@ public abstract class Protection : Iitem
         _armor = armor;
     }
 
+    //Devuelve el valor de la armadura
     public int GetArmor(){return _armor;}
+    
+    //Devuelve el nombre del item de protección
     public string GetName(){return _name;}
 
+    //Muestra información sobre el item de protección
     public void Apply(Character character)
     {
         Console.WriteLine($"{character._name} equiped {_name}, adding {_armor} points of defense");
--- Pruebas-CSharp/Weapon.cs
+++ Pruebas-CSharp/Weapon.cs
@@ -11,9 +11,13 @@ public abstract class Weapon : Iitem
         _damage = damage;
     }
     
+    //Devuelve el daño del arma
     public int GetDamage(){return _damage;}
+    
+    //devuelve el nombre del arma
     public string GetName(){return _name;}
 
+    //Muestra un mensaje sobre el arma
     public void Apply(Character character)
     {
         Console.WriteLine($"{character._name} equiped {_name}, adding {_damage} points of damage");
